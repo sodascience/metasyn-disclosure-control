0ce4f46387f00bd2eecf7b858f242fa9cd157fd8
Raoul Schram <r.d.schram@uu.nl>
2023-02-01 11:33:58 +0100

    Fix unique key distribution

--- metasynthcontrib/disclosure/discrete.py
+++ metasynthcontrib/disclosure/discrete.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-import numpy as np
 import polars as pl
 
 from metasynth.distribution.discrete import DiscreteUniformDistribution
@@ -10,6 +9,7 @@ from metasynth.distribution.discrete import PoissonDistribution
 from metasynth.distribution.discrete import UniqueKeyDistribution
 
 from metasynthcontrib.disclosure.numerical import DisclosureNumerical
+from metasynthcontrib.disclosure.utils import micro_aggregate
 
 
 class DisclosureDiscreteUniform(DiscreteUniformDistribution, DisclosureNumerical):
@@ -23,9 +23,9 @@ class DisclosureUniqueKey(UniqueKeyDistribution):
     def _fit(cls, values: pl.Series, n_avg: int=11):
         orig_dist = super()._fit(values)
         if orig_dist.consecutive == 1:
-            return cls(np.random.randint(2*n_avg+1)-n_avg, orig_dist.consecutive)
-        uniform_dist = DisclosureDiscreteUniform.fit(values, n_avg=n_avg)
-        return cls(uniform_dist.low, orig_dist.consecutive)  # type: ignore
+            return cls(0, 1)
+        sub_values = micro_aggregate(values, n_avg)
+        return super()._fit(sub_values)
 
 
 class DisclosurePoisson(PoissonDistribution, DisclosureNumerical):

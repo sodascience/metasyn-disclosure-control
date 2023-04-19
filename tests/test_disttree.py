f864a84d07876bcc22f559290d0dade1f206b2f8
Raoul Schram <r.d.schram@uu.nl>
2023-04-19 10:28:51 +0200

    Fix for new metasynth version

--- tests/test_disttree.py
+++ tests/test_disttree.py
@@ -1,11 +1,11 @@
 from pytest import mark
 
 from metasynth.testutils import check_dist_type
-from metasynth.disttree import get_disttree
+from metasynth.provider import get_distribution_provider
 
 
 @mark.parametrize(
-    "var_type", get_disttree().all_var_types)
+    "var_type", get_distribution_provider().all_var_types)
 def test_dist_tree(var_type):
     check_dist_type("disclosure", var_type, n_avg=10)
     check_dist_type("disclosure", var_type, n_avg=15)

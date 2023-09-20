dc3478ebf67d34b222fa9106e355cb4a47c98c2b
qubixes <44498096+qubixes@users.noreply.github.com>
2023-09-20 11:00:13 +0200

    Add string distribution with unique values

--- metasynthcontrib/disclosure/string.py
+++ metasynthcontrib/disclosure/string.py
@@ -1,6 +1,6 @@
 """Module for disclosure control for string distributions."""
 
-from metasynth.distribution.faker import FakerDistribution
+from metasynth.distribution.faker import FakerDistribution, UniqueFakerDistribution
 from metasynthcontrib.disclosure.base import metadist_disclosure
 
 
@@ -10,7 +10,19 @@ class DisclosureFaker(FakerDistribution):
 
     @classmethod
     def _fit(cls, values,
-             faker_type: str="city",
-             locale: str="en_US", n_avg=11):  # pylint: disable=unused-argument
+             faker_type: str = "city",
+             locale: str = "en_US", n_avg=11):  # pylint: disable=unused-argument
         return super(DisclosureFaker, cls)._fit(values, faker_type=faker_type,
                                                 locale=locale)
+
+
+@metadist_disclosure()
+class DisclosureUniqueFaker(UniqueFakerDistribution):
+    """Faker distribution for disclosure control that produces unique values."""
+
+    @classmethod
+    def _fit(cls, values,
+             faker_type: str = "city",
+             locale: str = "en_US", n_avg=11):  # pylint: disable=unused-argument
+        return super(DisclosureUniqueFaker, cls)._fit(values, faker_type=faker_type,
+                                                      locale=locale)

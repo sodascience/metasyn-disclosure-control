6de984cf8b82a11b66ac4102abf8d37624b253aa
Raoul Schram <r.d.schram@uu.nl>
2025-09-23 13:35:04 +0200

    Fix ruff

--- metasyncontrib/disclosure/__init__.py
+++ metasyncontrib/disclosure/__init__.py
@@ -1,6 +1,5 @@
 """Metasyn plugin that implements disclosure control."""
 
-from metasyncontrib.disclosure.privacy import DisclosurePrivacy
 from metasyncontrib.disclosure.categorical import DisclosureMultinoulli
 from metasyncontrib.disclosure.constant import (
     DisclosureConstant,
@@ -25,6 +24,7 @@ from metasyncontrib.disclosure.normal import (
     DisclosureTruncatedNormal,
 )
 from metasyncontrib.disclosure.poisson import DisclosurePoisson
+from metasyncontrib.disclosure.privacy import DisclosurePrivacy
 from metasyncontrib.disclosure.uniform import (
     DisclosureContinuousUniform,
     DisclosureDate,
